bc80dcd16efccc273890ee9030e5dac70ce2bf1c
Enzo Maçaneiro <127849398+Oznezinho@users.noreply.github.com>
2024-09-20 08:19:33 -0300

    Update and rename Main (2).java to Main.java

--- Aula03/Exemplo02/Main.java
+++ Aula03/Exemplo02/Main.java
@@ -2,9 +2,9 @@ public class Main {
   public static void main(String[] args) {
 
     // Instanciando o objeto Pessoa
-    Pessoa pessoa1 = new Pessoa("João", 25);
-    Pessoa pessoa2 = new Pessoa("Ana", 5);
-    Pessoa pessoa3 = new Pessoa("Vitor", 21);
+    Pessoa pessoa1 = new Pessoa("Enzo", 18);
+    Pessoa pessoa2 = new Pessoa("Bianca", 18);
+    Pessoa pessoa3 = new Pessoa("Nehyne", 2);
 
     System.out.println(pessoa1.exibirInfo());
     System.out.println(pessoa1.getNome());
